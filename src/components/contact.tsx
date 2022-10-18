9c7d7e632297cedb2ed6135a1067391754027f94
Leo Pereyra <leoerickp@gmail.com>
2022-10-17 23:30:41 -0400

    The send email module finished

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,9 +1,73 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import Global from "../Global";
+import SimpleReactValidator from "simple-react-validator";
+
+
 
 class Contact extends Component {
+    public emailRef = React.createRef();
+    public msgRef = React.createRef();
+    public validator = null;
+    state = {
+        message: {},
+        status: null
+    }
+    constructor(
+
+    ) {
+        super();
+        this.validator = new SimpleReactValidator({
+            messages: {
+                required: 'This field is required',
+                alpha: 'Do not accept numbers',
+            }
+        })
+    }
+    changeState = () => {
+        this.setState({
+            message: {
+                email: this.emailRef.current.value,
+                msg: this.msgRef.current.value,
+            },
+            status: "success",
+        });
+    }
+    sendMessage = (e) => {
+        e.preventDefault();
+        this.changeState();
+        //console.log(this.state.message);
+        axios.post(Global.url + 'sendmail', this.state.message)
+            .then(res => {
+                const { msg } = res.data;
+                if (msg == 'success') {
+                    alert('The message was sent successfully!')
+                }
+            })
+            .catch(err => {
+
+            });
+
+    }
+
     render(): React.ReactNode {
+        const message = this.state.message;
         return (
-            <h1>Contacto</h1>
+            <div className="flex justify-center items-center bg-cyan-200 shadow-lg border-2 p-2 border-cyan-300 rounded-md">
+                <form className="w-3/6" onSubmit={this.sendMessage}>
+                    <div className="flex flex-col gap-1 ">
+                        <label htmlFor="email" className="block text-md font-medium text-cyan-900">Email</label>
+                        <input type="text" name="email" className="bg-cyan-100 border-2 border-cyan-200 rounded-md shadow" defaultValue={message.email} onChange={this.changeState} ref={this.emailRef} required />
+                    </div>
+
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="content" className="block text-sm font-medium text-cyan-900">Message</label>
+                        <textarea name="content" className="bg-cyan-100 border-2 border-cyan-200 rounded-md shadow" defaultValue={message.msg} onChange={this.changeState} ref={this.msgRef} required ></textarea>
+                    </div>
+                    <div className=""></div>
+                    <input type="submit" value="Send" className="button-esp px-8 py-2 text-cyan-50 m-1" />
+                </form >
+            </div>
         );
     }
 }
